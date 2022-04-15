47913356276c9f735abd4624998d6315c81d1018
Joseph Champeau <jchampeau@wesleyan.edu>
2022-04-14 20:50:39 -0400

    Added song playing functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,11 @@ import './App.css';
 
 import React from 'react';
 import Song_lookup from './components/song_lookup';
-import Dubstep from './components/dubstep';
-import Epic from './components/Epic';
-import Sunny from './components/Sunny';
 
 function App() {
   return (
     <div className="App">
       <Song_lookup />
-      < Dubstep />
-      <Epic />
-      <Sunny/>
     </div>
   );
 
--- frontend/src/components/Song.js
+++ frontend/src/components/Song.js
@@ -17,15 +17,44 @@ import axios from "axios"
 export default class Song extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+
+      // Get audio file in a variable
+      audio: new Audio(this.props.file),
+  
+      // Set initial state of song
+      isPlaying: false,
+    };
   }
+
+  togglePlay = () => {
+    // Get state of song
+    let isPlaying = this.state.isPlaying;
+
+    if (isPlaying) {
+      // Pause the song if it is playing
+      this.state.audio.pause();
+    } else {
+      console.log("playing")
+      // Play the song if it is paused
+      this.state.audio.play();
+    }
+
+    // Change the state of song
+    this.setState({ isPlaying: !isPlaying });
+  };
 
   render() {
     return(
-    <li>
-      <span>{this.props.song} by {this.props.artist} is rated {this.props.average} ({this.props.count}) with file {this.props.file} </span>
+    <li style={{textAlign: "left"}}>
+      <span>{this.props.song} by {this.props.artist} is rated {this.props.average} ({this.props.count}) </span>
       <button onClick={() => this.props.onChange(this.props.id)}>
                   Edit
       </button>
+      <button onClick={this.togglePlay} style={{marginLeft: "0.5vw"}}>
+                  Play
+      </button>
+      <span style={{color: "green"}}>{this.state.isPlaying ? " Playing song!" : ""}</span>
     </li>)
   }
 }
